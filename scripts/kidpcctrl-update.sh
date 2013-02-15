631c8b3a1b390cac70211cb5f73c7aa25c85fcc1
Daniel Gerber <daniel.gerber2@gmail.com>
2013-02-15 15:25:07 +0100

    corrections

--- scripts/kidpcctrl-update.sh
+++ scripts/kidpcctrl-update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 USERXY=$(users | grep userxy)
-if [ -n "$DANIEL" ]; then
-  curl --data "username=Daniel" localhost:8080/kidpcctrl/rest/track/update
+if [ -n "$USERXY" ]; then
+  curl --data "username=<username>" localhost:8080/kidpcctrl/rest/track/update
 fi
 
 # add more users... as above
